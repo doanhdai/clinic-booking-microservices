e9c02e936cbfa91c1fda05feb31fe42a9f2aee2a
doanhdai <doanhdaigr5.2004@gmail.com>
2025-09-29 17:34:19 +0700

    first commit

---
+++ doctor-service/src/test/java/com/myclinic/doctor/DoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myclinic.doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
